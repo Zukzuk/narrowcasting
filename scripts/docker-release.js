7fa71450e9ab1b5bfca85ca5bfee5e85e72397a2
zukzuk <dave.timmerman@gmail.com>
2025-05-07 20:02:48 +0200

    fix: fixed issue where .env file is not loaded correctly

--- scripts/docker-release.js
+++ scripts/docker-release.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 // __dirname emulation in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, '../deploy/deploy.env') });
+dotenv.config({ path: path.resolve(__dirname, './deploy/deploy.env') });
 
 // Main function to orchestrate the script execution
 function main() {
@@ -24,7 +24,7 @@ function buildAndPushDockerImage(APP_VERSION_TAG, APP_PORT) {
   const imageVersionTag = `zukzuk/narrowcasting:${APP_VERSION_TAG}`;
   const imageLatestTag = `zukzuk/narrowcasting:latest`;
   try {
-    console.log("Building Docker release image...");
+    console.log(`docker build -f scripts/Dockerfile.release --build-arg APP_VERSION_TAG=${APP_VERSION_TAG} --build-arg APP_PORT=${APP_PORT} -t ${imageVersionTag} -t ${imageLatestTag} .`);
     execSync(`docker build -f scripts/Dockerfile.release --build-arg APP_VERSION_TAG=${APP_VERSION_TAG} --build-arg APP_PORT=${APP_PORT} -t ${imageVersionTag} -t ${imageLatestTag} .`, { stdio: 'inherit' });
     console.log(`Pushing '${APP_VERSION_TAG}' and 'latest' to Docker registry...`);
     execSync(`docker push ${imageVersionTag}`, { stdio: 'inherit' });
