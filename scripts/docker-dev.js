036666e89ac3f35748c8a8a968081d730231421b
zukzuk <dave.timmerman@gmail.com>
2025-05-07 20:05:36 +0200

    fix: fixed filename reference

--- scripts/docker-dev.js
+++ scripts/docker-dev.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 // __dirname emulation in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, '../deploy/deploy.env') });
+dotenv.config({ path: path.resolve(__dirname, '../deploy/public.env') });
 dotenv.config({ path: path.resolve(__dirname, '../dev.env') });
 
 // Main function to orchestrate the script execution
